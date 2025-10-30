e8c4b3127d9d5353c8a4e79321c2b88db98ccfe2
GuilhermeLossio <86495756+GuilhermeLossio@users.noreply.github.com>
2025-10-29 23:01:11 -0300

    Update on requirements.txt

--- services/api/app.py
+++ services/api/app.py
@@ -11,7 +11,7 @@ import sys
 import os
 
 sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from services.resources.Extract import *
 
